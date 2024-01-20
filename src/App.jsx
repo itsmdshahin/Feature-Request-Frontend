2d7dcbdcdb00ae6532b2e7474ad9eee3f3193276
itsmdshahin <mdshahinurrahman8@gmail.com>
2024-01-20 18:56:16 +0600

    update for live

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // frontend/src/App.jsx
 import React from 'react'; 
-import Routes from './Routes/Routes';
+import Routes from './routes/Routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
